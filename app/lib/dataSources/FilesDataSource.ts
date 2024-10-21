0c09b81f87153cccc33804180cde890f3df4c85f
bright nkhoma <gis-020-20@must.ac.mw>
2024-10-21 10:14:45 +0200

    mis

--- app/lib/dataSources/FilesDataSource.ts
+++ app/lib/dataSources/FilesDataSource.ts
@@ -8,7 +8,7 @@ import { getStorage,ref,uploadBytesResumable,getDownloadURL,deleteObject } from 
 //     console.log(data);
     
 // }
-const url = "https://misoapi-l8iw-fs7aflb0p-bright-nkhomas-projects.vercel.app/"
+const url = "https://misoapi-psi.vercel.app/"
 export interface CommitResult{
     status : boolean,
     message : string
