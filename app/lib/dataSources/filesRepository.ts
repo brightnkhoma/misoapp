456ea7737a63e763a204bc9fd074936a9221928d
bright nkhoma <gis-020-20@must.ac.mw>
2024-10-26 12:58:07 +0200

    repo

--- app/lib/dataSources/filesRepository.ts
+++ app/lib/dataSources/filesRepository.ts
@@ -25,9 +25,9 @@ export async function getCompletedFiles(onSuccess : (data : Array<MisoCompletedF
     const dataSource = new MisoFileDataSource()
     await dataSource.fetchCompleteData(onSuccess,onFailure)
 }
-export async function processData(name : string, path : string,onSuccess : (data : string) => void, onFailure : (data : CommitResult)=> void){
+export async function processData(name : string, path : string,ref : MisoFile,onSuccess : (data : string) => void, onFailure : (data : CommitResult)=> void){
     const dataSource = new MisoFileDataSource()
-    await dataSource.process(name,path,onSuccess,onFailure)
+    await dataSource.process(name,path,ref,onSuccess,onFailure)
 }
 export async function processDataRef(name : string, path : string,onSuccess : (data : string) => void, onFailure : (data : CommitResult)=> void){
     const dataSource = new MisoFileDataSource()
